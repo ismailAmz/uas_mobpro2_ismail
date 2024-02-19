f5e3d74c79e8167a28d4755c8fee0cae13a9d56e
ismailAmz <ismailalmuzamal1@gmail.com>
2024-02-19 09:52:32 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/uas_mobpro2_ismail/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uas_mobpro2_ismail
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/todo_list.dart
@@ -0,0 +1,145 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:uas_mobpro2_ismail/screens/add_page.dart';
+import 'package:http/http.dart' as http;
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  bool isLoading = true;
+  List items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: Visibility(
+        visible: isLoading,
+        child: Center(child: CircularProgressIndicator()),
+        replacement: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index] as Map;
+              final id = item['_id'] as String;
+              return ListTile(
+                leading: CircleAvatar(child: Text('${index + 1}')),
+                title: Text(item['title']),
+                subtitle: Text(item['description']),
+                trailing: PopupMenuButton(onSelected: (value) {
+                  if (value == 'edit') {
+                    // open Edit page
+                    navigateToEditPage(item);
+                  } else if (value == 'delete') {
+                    // delete and remove the item
+                    deleteById(id);
+                  }
+                }, itemBuilder: (context) {
+                  return [
+                    PopupMenuItem(
+                      child: Text('Edit'),
+                      value: 'edit',
+                    ),
+                    PopupMenuItem(
+                      child: Text('Delete'),
+                      value: 'delete',
+                    ),
+                  ];
+                }),
+              );
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: navigateToAddPage,
+        label: Text('Add Todo'),
+      ),
+    );
+  }
+
+  Future<void> navigateToEditPage(Map item) async {
+    final route =
+        MaterialPageRoute(builder: (context) => AddTodoPage(todo: item));
+    Navigator.push(context, route);
+    // await Navigator.push(context, route);
+    // setState(() {
+    //   isLoading = true;
+    // });
+    // fetchTodo();
+  }
+
+  Future<void> navigateToAddPage() async {
+    final route = MaterialPageRoute(builder: (context) => AddTodoPage());
+    await Navigator.push(context, route);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
+  }
+
+  Future<void> deleteById(String id) async {
+    // delete the item
+    final url = 'https://api.nstack.in/v1/todos/$id';
+    final uri = Uri.parse(url);
+    final response = await http.delete(uri);
+    if (response.statusCode == 200) {
+      // remove item from the list
+      final filtered = items.where((element) => element['_id'] != id).toList();
+      setState(() {
+        items = filtered;
+      });
+    } else {
+      // show error
+      showErrorMassage('Deletion Failed');
+    }
+    // Remove item form the list
+  }
+
+  Future<void> fetchTodo() async {
+    final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['items'] as List;
+      setState(() {
+        items = result;
+      });
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  void showSuccessMassage(String message) {
+    final snackbar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
+  void showErrorMassage(String message) {
+    final snackbar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+}
